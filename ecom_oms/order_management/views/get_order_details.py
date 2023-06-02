96c8e779a041dab140715aa0156dd8797d95bd7b
Hitesh <hitesh93kr@gmail.com>
2023-06-03 01:31:31 +0530

    Add order items in order listing and details API

--- ecom_oms/order_management/views/get_order_details.py
+++ ecom_oms/order_management/views/get_order_details.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from order_management.models import Order
+from order_management.models import Order, OrderItem
 
 
 class GetOrderDetailsView(APIView):
@@ -14,13 +14,33 @@ class GetOrderDetailsView(APIView):
             return Response({"status": 0,
                              "message": "Order does not exist, please check the order ID."})
 
+        try:
+
+            order_items = OrderItem.objects.filter(order=order_id)
+        except OrderItem.DoesNotExist:
+            return Response({"status": 0,
+                             "message": "Order does not have any associated items."})
+
         details = {
             "order_id": order.id,
             "user_email": order.user.email,
             "order_status": order.order_status,
             "created_date": self.getTimeDateAsStr(order.created_at),
             "updated_date": self.getTimeDateAsStr(order.updated_at),
+            "items": []
         }
+
+        for item in order_items:
+            res = {
+                "name" : item.name,
+                "quantity" : item.quantity,
+                "price" : item.price,
+                "created_date": self.getTimeDateAsStr(item.created_at),
+                "updated_date": self.getTimeDateAsStr(item.updated_at)
+            }
+
+            details["items"].append(res)
+
 
         return Response({"status": 1,
                          "details": details})
--- ecom_oms/order_management/views/order_listing.py
+++ ecom_oms/order_management/views/order_listing.py
@@ -1,15 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from order_management.models import Order
+from order_management.models import Order, OrderItem
 
 
 class OrderListingView(APIView):
     """ Order listing view """
 
     def get(self, request):
-        order_list = Order.objects.all()
+        order_list = Order.objects.all().order_by("created_at")
 
-        res = []
+        data = []
         for order in order_list:
             details = {
                 "order_id": order.id,
@@ -17,12 +17,30 @@ class OrderListingView(APIView):
                 "order_status": order.order_status,
                 "created_date": self.getTimeDateAsStr(order.created_at),
                 "updated_date": self.getTimeDateAsStr(order.updated_at),
+                "items": []
             }
 
-            res.append(details)
+            try:
+
+                order_items = OrderItem.objects.filter(order=order.id)
+            except OrderItem.DoesNotExist:
+                continue
+
+            for item in order_items:
+                res = {
+                    "name" : item.name,
+                    "quantity" : item.quantity,
+                    "price" : item.price,
+                    "created_date": self.getTimeDateAsStr(item.created_at),
+                    "updated_date": self.getTimeDateAsStr(item.updated_at)
+                }
+
+                details["items"].append(res)
+
+            data.append(details)
 
         return Response({"status": 1,
-                         "data": res})
+                         "data": data})
 
     def getTimeDateAsStr(cls, time, format="%B %d, %Y"):
         """ Get time date as str """
